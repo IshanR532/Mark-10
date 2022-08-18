6bbe4c00b897e4cf271086e9915dc8fa58857150
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-08-18 23:52:02 +0530

    not up to mark

---
+++ index.js
@@ -0,0 +1,13 @@
+const billAmount = document.querySelector("#bill-amount");
+
+console.log(billAmount);
+
+//const checkButton = document.querySelector("#check-button")
+
+//checkButton.addEventListener('click',()=>{console.log(billAmount.value)})
+
+
+
+
+//const checkButton = document.querySelector("#check-button");
+
