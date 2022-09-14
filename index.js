77a1ecd06a5b98d869e9a9babb5182f2b8efbf0f
Ishan Udgirkar <udgirkarishan@gmail.com>
2022-09-15 00:43:52 +0530

    Sucessful !

--- index.js
+++ index.js
@@ -4,25 +4,43 @@ const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000,500,100,20,10,5,1];
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkButton.addEventListener("click",function validateBillAndCashAmount(){
-    hideMessage();
-    if(billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        }else{
-            showMessage("The cash given is insufficient");
+cashGiven.style.display = "none";
+
+
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    let billAmountNum = Number(billAmount.value);
+    let cashGivenNum = Number(cashGiven.value);
+
+    if (billAmountNum) {
+        hideMessage();
+        if (billAmountNum > 0) {
+            cashGiven.style.display = "block";
+            if (cashGivenNum === 0) {
+                showMessage("Enter cash given")
+            } else if (cashGivenNum < 0) {
+                showMessage("Enter only positive value for cash")
+            } else {
+                if (cashGivenNum >= billAmountNum) {
+                    const amountToBeReturned = cashGivenNum - billAmountNum;
+                    calculateChange(amountToBeReturned);
+                } else {
+                    showMessage("The cash given is insufficient");
+                }
+            }
+        } else {
+            showMessage("Invalid Bill Amount");
         }
-    }else{
-        showMessage("Invalid Bill Amount");
+    } else {
+        showMessage("Please Enter Bill Amount !");
     }
 });
 
-function calculateChange(amountToBeReturned){
-    
-    for(let i = 0 ; i < availableNotes.length ; i++ ){
+function calculateChange(amountToBeReturned) {
+
+    for (let i = 0; i < availableNotes.length; i++) {
 
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
@@ -34,8 +52,8 @@ function calculateChange(amountToBeReturned){
     }
 }
 
-function hideMessage(){
-   message.style.display = "none";
+function hideMessage() {
+    message.style.display = "none";
 }
 
 
@@ -44,5 +62,3 @@ function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
 }
-
-
